0f3608dd2060547600da636e50755d10fc9479c1
Andrew Fitzgerald <andrewcfitzgerald@gmail.com>
2019-11-22 04:27:23 -0500

    dimensional app updates + demonstrative tests

--- 2-dimensions/src/main/java/codemash/DimensionalCounterApplication.java
+++ 2-dimensions/src/main/java/codemash/DimensionalCounterApplication.java
@@ -5,17 +5,19 @@ import io.micrometer.prometheus.PrometheusMeterRegistry;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.http.MediaType;
-import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@EnableScheduling
 @RestController
 @SpringBootApplication
 public class DimensionalCounterApplication {
 
-    private final PrometheusMeterRegistry registry = new PrometheusMeterRegistry(PrometheusConfig.DEFAULT);
+    PrometheusMeterRegistry meterRegistry;
+
+    public DimensionalCounterApplication() {
+        meterRegistry = new PrometheusMeterRegistry(PrometheusConfig.DEFAULT);
+        meterRegistry.config().commonTags("conference", "codemash");
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(DimensionalCounterApplication.class, args);
@@ -23,22 +25,9 @@ public class DimensionalCounterApplication {
 
     @GetMapping(value = {"/", "/scrape"}, produces = MediaType.TEXT_PLAIN_VALUE)
     public String scrape() {
-        return registry.scrape();
-    }
-
-
-    @Scheduled(fixedRate = 1)
-    public void incrementOneMilli() {
-        registry.counter("dimensional", "period", "1ms").increment();
-    }
-
-    @Scheduled(fixedRate = 1000)
-    public void incrementOneSec() {
-        registry.counter("dimensional", "period", "1s").increment();
-    }
-
-    @Scheduled(fixedRate = 5000)
-    public void incrementFiveSec() {
-        registry.counter("dimensional", "period", "5s").increment();
+        meterRegistry.counter("dimensional", "increment-by", "one").increment(1);
+        meterRegistry.counter("dimensional", "increment-by", "two").increment(2);
+        meterRegistry.counter("dimensional", "increment-by", "three").increment(3);
+        return meterRegistry.scrape();
     }
 }
---
+++ 2-dimensions/src/test/java/codemash/DimensionalCounterApplicationTest.java
@@ -0,0 +1,61 @@
+package codemash;
+
+import io.micrometer.core.instrument.Counter;
+import io.micrometer.core.instrument.MeterRegistry;
+import io.micrometer.core.instrument.simple.SimpleMeterRegistry;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class DimensionalCounterApplicationTest {
+
+    @Test
+    void sameNameAndTagsAreSame() {
+        MeterRegistry registry = new SimpleMeterRegistry();
+        Counter counter1 = registry.counter("counter", "key", "value");
+        Counter counter2 = registry.counter("counter", "key", "value");
+
+        assertThat(counter1).isSameAs(counter2);
+    }
+
+    @Test
+    void differentTagsMeansDifferentMeters() {
+        MeterRegistry registry = new SimpleMeterRegistry();
+        Counter counter1 = registry.counter("counter", "key", "value1");
+        Counter counter2 = registry.counter("counter", "key", "value2");
+
+        assertThat(counter1).isNotEqualTo(counter2);
+    }
+
+    @Test
+    void canHaveCommonTags() {
+        MeterRegistry registry = new SimpleMeterRegistry();
+        registry.config().commonTags("conference", "codemash");
+        Counter counter = registry.counter("counter", "key", "value");
+
+        assertThat(counter.getId().getTags()).hasSize(2);
+    }
+
+    @Test
+    void orderingMatters() {
+        MeterRegistry registry = new SimpleMeterRegistry();
+        Counter counter1 = registry.counter("counter", "key", "value");
+        registry.config().commonTags("conference", "codemash");
+        Counter counter2 = registry.counter("counter", "key", "value");
+
+
+        assertThat(counter1.getId().getTags()).hasSize(1);
+        assertThat(counter2.getId().getTags()).hasSize(2);
+        assertThat(counter1).isNotEqualTo(counter2);
+    }
+
+    @Test
+    void canAddTagsWithTheLongFormBuilder() {
+        MeterRegistry registry = new SimpleMeterRegistry();
+        Counter.builder("counter")
+                .tag("key", "value")
+                .description("some kind of counter")
+                .baseUnit("things")
+                .register(registry);
+    }
+}
