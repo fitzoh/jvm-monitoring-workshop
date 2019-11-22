725e0432192066ad02aeb4cac5af8d2fbea83e48
Andrew Fitzgerald <andrewcfitzgerald@gmail.com>
2019-11-22 02:20:46 -0500

    rename first module and simplify a bit

--- 1-simple-e2e/src/main/java/codemash/EndToEndCounterApplication.java
+++ 1-simple-e2e/src/main/java/codemash/EndToEndCounterApplication.java
@@ -1,33 +1,28 @@
 package codemash;
 
+import io.micrometer.core.instrument.Counter;
 import io.micrometer.prometheus.PrometheusConfig;
 import io.micrometer.prometheus.PrometheusMeterRegistry;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.http.MediaType;
-import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@EnableScheduling
 @RestController
 @SpringBootApplication
-public class SimpleCounterApplication {
+public class EndToEndCounterApplication {
 
-    private final PrometheusMeterRegistry registry = new PrometheusMeterRegistry(PrometheusConfig.DEFAULT);
+    PrometheusMeterRegistry meterRegistry = new PrometheusMeterRegistry(PrometheusConfig.DEFAULT);
+    Counter counter = meterRegistry.counter("simple");
 
     public static void main(String[] args) {
-        SpringApplication.run(SimpleCounterApplication.class, args);
+        SpringApplication.run(EndToEndCounterApplication.class, args);
     }
 
     @GetMapping(value = {"/", "/scrape"}, produces = MediaType.TEXT_PLAIN_VALUE)
     public String scrape() {
-        return registry.scrape();
-    }
-
-    @Scheduled(fixedRate = 1000)
-    public void increment() {
-        registry.counter("simple").increment();
+        counter.increment();
+        return meterRegistry.scrape();
     }
 }
