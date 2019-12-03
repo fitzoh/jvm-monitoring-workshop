ea6a6b0959650daff4fa1e5280e33c176d5b8a18
Andrew Fitzgerald <andrewcfitzgerald@gmail.com>
2019-12-02 22:44:03 -0500

    reformat registry demo + increase log rate

--- 3-registries/src/main/java/codemash/RegistryApplication.java
+++ 3-registries/src/main/java/codemash/RegistryApplication.java
@@ -1,6 +1,7 @@
 package codemash;
 
 import io.micrometer.core.instrument.Clock;
+import io.micrometer.core.instrument.Counter;
 import io.micrometer.core.instrument.composite.CompositeMeterRegistry;
 import io.micrometer.core.instrument.logging.LoggingMeterRegistry;
 import io.micrometer.core.instrument.logging.LoggingRegistryConfig;
@@ -22,9 +23,16 @@ public class RegistryApplication {
     PrometheusMeterRegistry prometheusMeterRegistry = new PrometheusMeterRegistry(PrometheusConfig.DEFAULT);
     LoggingMeterRegistry loggingMeterRegistry = new LoggingMeterRegistry(loggingConfig(), Clock.SYSTEM);
 
+    Counter counter;
+
     public RegistryApplication() {
         compositeMeterRegistry.add(prometheusMeterRegistry);
         compositeMeterRegistry.add(loggingMeterRegistry);
+        counter = Counter.builder("simple")
+                .description("a simple counter")
+                .baseUnit("thing")
+                .tag("conference", "codemash")
+                .register(compositeMeterRegistry);
     }
 
     public static void main(String[] args) {
@@ -34,7 +42,7 @@ public class RegistryApplication {
 
     @GetMapping(value = {"/", "/scrape"}, produces = MediaType.TEXT_PLAIN_VALUE)
     public String scrape() {
-        compositeMeterRegistry.counter("simple").increment();
+        counter.increment();
         return prometheusMeterRegistry.scrape();
     }
 
@@ -47,7 +55,7 @@ public class RegistryApplication {
 
             @Override
             public Duration step() {
-                return Duration.ofSeconds(5);
+                return Duration.ofSeconds(1);
             }
         };
     }
