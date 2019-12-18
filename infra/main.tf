7931656d8847a27a9365674943a0946512c00649
Andrew Fitzgerald <andrewcfitzgerald@gmail.com>
2019-12-18 13:31:55 -0500

    lb needs egress

--- infra/main.tf
+++ infra/main.tf
@@ -33,10 +33,17 @@ data "aws_subnet_ids" "default" {
 }
 
 resource "aws_security_group" "http_ingress" {
+  name = "lb-ingress"
   ingress {
     from_port   = 80
     protocol    = "tcp"
     to_port     = 80
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port = 0
+    protocol = "tcp"
+    to_port = 0
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
